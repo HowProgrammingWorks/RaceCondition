1d2f24ea41cf3f9248ad99684e9c415ac64a2889
tshemsedinov <timur.shemsedinov@gmail.com>
2019-08-17 16:22:40 +0300

    Try to solve problem with async/await

---
+++ JavaScript/4-async-await.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const fs = require('fs').promises;
+
+// Implementation
+
+const warehouse = {
+  shipped: 0,
+
+  async ship(product, quantity) {
+    console.log(`Request ${quantity} ${product}`);
+    const fileName = `./data/${product}.json`;
+    const json = await fs.readFile(fileName, 'utf8');
+    const record = JSON.parse(json);
+    const balance = record.quantity - quantity;
+    if (balance <= 0) {
+      console.log(`Can't ship ${quantity} ${product}`);
+      return false;
+    }
+    record.quantity = balance;
+    this.shipped += quantity;
+    const result = JSON.stringify(record);
+    await fs.writeFile(fileName, result);
+    console.log(`Shipped ${quantity} ${product}`);
+    console.log(`Shipped total: ${this.shipped}; Balance: ${balance}`);
+    return true;
+  }
+};
+
+// Usage
+
+(async () => {
+  await warehouse.ship('glass', 20);
+  await warehouse.ship('glass', 10);
+  await warehouse.ship('glass', 20);
+  await warehouse.ship('glass', 20);
+  await warehouse.ship('glass', 70);
+  await warehouse.ship('glass', 10);
+  await warehouse.ship('glass', 50);
+  await warehouse.ship('glass', 10);
+})();
