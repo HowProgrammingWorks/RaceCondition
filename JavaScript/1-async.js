ed7e4c7de61fae1acaf0edd32154899f0bfad5b7
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-06-01 17:20:14 +0300

    Add hint

--- JavaScript/1-async.js
+++ JavaScript/1-async.js
@@ -25,6 +25,9 @@ class Point {
 const p1 = new Point(10, 10);
 console.log(p1);
 
+// Hint: Imagine that this 4 calls are from different places
+// (different client requests, different timer events, etc.)
+// so we can't just add await before each call, we need to protect point
 p1.move(5, 5);
 p1.move(6, 6);
 p1.move(7, 7);
